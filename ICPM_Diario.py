80d24e5053f0d4d83f166e00ecc1f7db05c9614a
Tokamakbr <souza.pmd@gmail.com>
2019-11-07 09:20:19 -0300

    Cria cálculo do ICPM diário partindo das ordens do dia

--- ICPM_Diario.py
+++ ICPM_Diario.py
@@ -1,20 +1,35 @@
 
-#-Begin-----------------------------------------------------------------
+# -Begin-----------------------------------------------------------------
 
-#-Includes--------------------------------------------------------------
+# -Includes--------------------------------------------------------------
 import sys
 import win32com.client
 import os
 import datetime
 import xlwings as xw
+import easygui
+import pandas as pd
 
 
-# -Caminho da pasta de Salvamento
-#V:\COMPARTILHADO_CSC-SSE_NSIF\NP-2\GPI\4 - Apoio Administrativo\4.4 - Monte Albuquerque\Desenvolvimento\Pedro\Pycharm\ICPM Diário\Parcial
+# -Caminho da pasta de Salvamento V:\COMPARTILHADO_CSC-SSE_NSIF\NP-2\GPI\4 - Apoio Administrativo\4.4 - Monte
+# Albuquerque\Desenvolvimento\Pedro\Pycharm\ICPM Diário\Parcial
+
 c_pasta = r"V:\COMPARTILHADO_CSC-SSE_NSIF\NP-2\GPI\4 - Apoio Administrativo\4.4 - Monte " \
           r"Albuquerque\Desenvolvimento\Pedro\Pycharm\ICPM Diário\Parcial"
 n_file = "ICPM_" + datetime.datetime.today().strftime("%Y%m%d") + ".xlsx"
 c_file = os.path.join(c_pasta, n_file)
+
+# Abrir fonte dos dados das ordens (https://stackoverflow.com/questions/17977540/pandas-looking-up-the-list-of-sheets-in-an-excel-file)
+path_fonte_dados = easygui.fileopenbox()
+xl = pd.ExcelFile(path_fonte_dados)
+aba = easygui.buttonbox('Escolha a aba a carregar', 'Abas:', xl.sheet_names)
+dados = xl.parse(aba, skiprows=18)
+dados['OM'].to_clipboard(excel=True, sep=None, index=False, header=False)
+
+
+
+
+
 
 #-Sub Main--------------------------------------------------------------
 def Main():
@@ -78,31 +93,7 @@ def Main():
         session.findById("wnd[1]/tbar[0]/btn[0]").press()
         print("criado arquivo em " + c_file)
 
-    # Editar o Excel
 
-    # Start by opening the spreadsheet and selecting the main sheet
-    workbook = load_workbook(filename=c_file)
-    sheet = workbook.active
-
-    # Write what you want into a specific cell
-    sheet.insert_cols(idx=1)
-
-
-    sheet["C1"] = "writing ;)"
-
-    # Save the spreadsheet
-    workbook.save(filename=c_file)
-
-    xw.books.open(c_file)
-    xw.Range("A:A").select()
-    xw.
-    xw.Shift := xlToRight
-    xw.Range("A2").Select
-    xw.ActiveCell.FormulaR1C1 = "=RC[3]&""/""&VALUE(RC[4])"
-    xw.Range("A2").Select
-    xw.Selection.AutoFill
-    xw.Destination := Range("A2:A70")
-    xw.Range("A2:A70").Select
 
   except:
     print(sys.exc_info()[0])
@@ -122,45 +113,3 @@ Main()
 
 
 
-
-
-#Macro Excel
-
-    Columns("A:A").Select
-    Selection.Insert Shift:=xlToRight
-    Range("A2").Select
-    ActiveCell.FormulaR1C1 = "=RC[3]&""/""&VALUE(RC[4])"
-    Range("A2").Select
-    Selection.AutoFill Destination:=Range("A2:A70")
-    Range("A2:A70").Select
-
-
-
-# Texto do SAp
-
-
-# session.findById("wnd[0]/tbar[0]/okcd").text = "iw37n"
-# session.findById("wnd[0]").sendVKey(0)
-# session.findById("wnd[0]/usr/tabsTABSTRIP_TABBLOCK1/tabpS_TAB1/ssub%_SUBSCREEN_TABBLOCK1:RI_ORDER_OPERATION_LIST:1100/ctxtS_DATUM-LOW").text = ""
-# session.findById("wnd[0]").sendVKey(0)
-# session.findById("wnd[0]/usr/tabsTABSTRIP_TABBLOCK1/tabpS_TAB1/ssub%_SUBSCREEN_TABBLOCK1:RI_ORDER_OPERATION_LIST:1100/btn%_S_AUFNR_%_APP_%-VALU_PUSH").press()
-# session.findById("wnd[1]/tbar[0]/btn[24]").press()
-# session.findById("wnd[1]/tbar[0]/btn[8]").press()
-# session.findById("wnd[0]/usr/tabsTABSTRIP_TABBLOCK1/tabpS_TAB9").select()
-# session.findById("wnd[0]/usr/chkSP_MAB").selected = -1
-# session.findById("wnd[0]/usr/tabsTABSTRIP_TABBLOCK1/tabpS_TAB9/ssub%_SUBSCREEN_TABBLOCK1:RI_ORDER_OPERATION_LIST:1900/ctxtSP_VARI").text = "/MD_progsem"
-# session.findById("wnd[0]/usr/chkSP_MAB").setFocus()
-# session.findById("wnd[0]/tbar[1]/btn[8]").press()
-# session.findById("wnd[0]/usr/cntlGRID1/shellcont/shell").currentCellRow = 2
-# session.findById("wnd[0]/usr/cntlGRID1/shellcont/shell").selectAll()
-# session.findById("wnd[0]/usr/cntlGRID1/shellcont/shell").contextMenu()
-# session.findById("wnd[0]/usr/cntlGRID1/shellcont/shell").selectContextMenuItem("&XXL")
-# session.findById("wnd[1]/tbar[0]/btn[0]").press()
-# session.findById("wnd[1]/usr/ctxtDY_PATH").setFocus()
-# session.findById("wnd[1]/usr/ctxtDY_PATH").caretPosition = 0
-# session.findById("wnd[1]").sendVKey(4)
-# session.findById("wnd[2]/usr/ctxtDY_PATH").text = "V:\COMPARTILHADO_CSC-SSE_NSIF\NP-2\GPI\4 - Apoio Administrativo\4.4 - Monte Albuquerque\Desenvolvimento\Pedro\Pycharm\ICPM Diário\Parcial"
-# session.findById("wnd[2]/usr/ctxtDY_FILENAME").text = "ICPM_20191106.xlsx"
-# session.findById("wnd[2]/usr/ctxtDY_FILENAME").caretPosition = 18
-# session.findById("wnd[2]/tbar[0]/btn[11]").press()
-# session.findById("wnd[1]/tbar[0]/btn[0]").press()
