05a61049be7f23f0832996b90621d13f3d7a1945
Tokamakbr <souza.pmd@gmail.com>
2019-12-10 14:33:44 -0300

    Script para Anexar Ordem com Interface com usuário

--- Scrapper_Compras.py
+++ Scrapper_Compras.py
@@ -25,18 +25,18 @@ time.sleep(5) # Let the user actually see something!
 input("Feche todos os avisos PVF")
 
 
-texto=[]
-tcompleto=[]
+products=[] #List to store name of the product
+prices=[] #List to store price of the product
+ratings=[] #List to store rating of the product
+
 content = driver.page_source
-soup = BeautifulSoup(content)
+soup = BeautifulSoup(content, 'html.parser')
 
+print(soup.title)
+for a in soup.find_all("div", attrs={'class':'jsonHide'}):
+    products.append(a.text)
 
+b = products[0]
+requisicao = b[b.find('requisicao')+13:b.find('requisicao')+21]
+entrega = b[b.find('"entrega"')+11:b.find('"entrega"')+11+8]
 
-for a in soup.findAll('timecard',href=True, attrs={'class':'display dataTable no-footer'}):
-    texto=a.find('div', attrs={'class':'ul1'})
-    tcompleto = a.find('div', attrs={'class': 'circ_completo'})
-    #price=a.find('div', attrs={'class':'timecard'})
-    #rating=a.find('div', attrs={'class':'hGSR34 _2beYZw'})
-    texto.append(texto.text)
-    tcompleto.append(tcompleto.text)
-    #ratings.append(rating.text)
